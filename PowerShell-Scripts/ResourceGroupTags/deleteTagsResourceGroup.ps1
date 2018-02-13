c8a7dc282aeb3a740eb0ad9f4ad3a0c26cf7f4be
Fernando Mejia <femeji@microsoft.com>
2018-02-13 15:07:50 -0300

    eliminacion de etiquetas

--- PowerShell-Scripts/ResourceGroupTags/deleteTagsResourceGroup.ps1
+++ PowerShell-Scripts/ResourceGroupTags/deleteTagsResourceGroup.ps1
@@ -1,22 +1,10 @@
 ##variable declaration
-$resourceGroupName = "bot-feria-adessa"
-$tagsHashTable = @{ Dept="IT"; Environment="Test" }
+$resourceGroupName = "demoResourceGroup"
+$tagsHashTable = @{}
 
 ## To add tags to a resource group without existing tags
 Set-AzureRmResourceGroup -Name $resourceGroupName -Tag $tagsHashTable
 
 ##To apply all tags from a resource group to its resources, and retain existing tags on resources that are not duplicates, use the following script
-$group = Get-AzureRmResourceGroup $resourceGroupName
-if ($group.Tags -ne $null) {
-    $resources = $group | Find-AzureRmResource
-    foreach ($r in $resources)
-    {
-        $resourcetags = (Get-AzureRmResource -ResourceId $r.ResourceId).Tags
-        foreach ($key in $group.Tags.Keys)
-        {
-            if (($resourcetags) -AND ($resourcetags.ContainsKey($key))) { $resourcetags.Remove($key) }
-        }
-        $resourcetags += $group.Tags
-        Set-AzureRmResource -Tag $resourcetags -ResourceId $r.ResourceId -Force
-    }
-}
+$g = Get-AzureRmResourceGroup -Name $resourceGroupName
+Find-AzureRmResource -ResourceGroupNameEquals $g.ResourceGroupName | ForEach-Object {Set-AzureRmResource -ResourceId $_.ResourceId -Tag $g.Tags -Force }

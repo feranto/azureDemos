c120dcffa932f2ebe47fcd95141fbfda05de58f8
Fernando Mejia <femeji@microsoft.com>
2018-02-13 14:54:22 -0300

    agregacion de tags exitoso

--- PowerShell-Scripts/ResourceGroupTags/addTagsResourceGroupWithoutTags.ps1
+++ PowerShell-Scripts/ResourceGroupTags/addTagsResourceGroupWithoutTags.ps1
@@ -1,13 +1,22 @@
 ##variable declaration
-$resourceGroupName = "examplegroup"
+$resourceGroupName = "bot-feria-adessa"
 $tagsHashTable = @{ Dept="IT"; Environment="Test" }
 
 ## To add tags to a resource group without existing tags
 Set-AzureRmResourceGroup -Name $resourceGroupName -Tag $tagsHashTable
 
-##To apply all tags from a resource group to its resources, and not retain existing tags on the resources, use the following script
-$groups = Get-AzureRmResourceGroup
-foreach ($g in $groups)
-{
-    Find-AzureRmResource -ResourceGroupNameEquals $g.ResourceGroupName | ForEach-Object {Set-AzureRmResource -ResourceId $_.ResourceId -Tag $g.Tags -Force }
+##To apply all tags from a resource group to its resources, and retain existing tags on resources that are not duplicates, use the following script
+$group = Get-AzureRmResourceGroup $resourceGroupName
+if ($group.Tags -ne $null) {
+    $resources = $group | Find-AzureRmResource
+    foreach ($r in $resources)
+    {
+        $resourcetags = (Get-AzureRmResource -ResourceId $r.ResourceId).Tags
+        foreach ($key in $group.Tags.Keys)
+        {
+            if (($resourcetags) -AND ($resourcetags.ContainsKey($key))) { $resourcetags.Remove($key) }
+        }
+        $resourcetags += $group.Tags
+        Set-AzureRmResource -Tag $resourcetags -ResourceId $r.ResourceId -Force
+    }
 }
